bb4be900bfe2b7aaca1cc954fc9f81f280f2f6bc
Ternia Wilson <terniawilson@Ternias-MacBook-Pro.local>
2020-02-06 18:57:02 -0500

    changes

---
+++ benchmarkproject.cpp
@@ -0,0 +1,18 @@
+#include <stdint.h>
+#include <cmath>
+#include <time.h>
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+clock_t intTime;
+    intTime = clock();
+    //32-bit integer operation benchmark
+    int32_t n = 0;
+    int32_t m = 0;
+    int32_t o = 0;         
+
+
+}
