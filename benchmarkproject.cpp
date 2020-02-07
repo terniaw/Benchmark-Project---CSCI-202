8241b11016928b0027663007d01e54db603bc5c7
Ternia Wilson <terniawilson@Ternias-MacBook-Pro.local>
2020-02-06 19:40:31 -0500

    changes, more

--- benchmarkproject.cpp
+++ benchmarkproject.cpp
@@ -1,29 +1,24 @@
-#include <stdint.h>
-#include <cmath>
-#include <time.h>
 #include <iostream>
-using namespace std;
+#include <stdint.h>
+#include <time.h>
+#include <math.h>
 
 int main()
-
 {
-clock_t intTime;
+    
+    clock_t intTime;
     intTime = clock();
     //32-bit integer operation benchmark
     int32_t n = 0;
     int32_t m = 0;
-    int32_t o = 0;         
-
-    for(int64_t i = 0; i < 100000000000; i++)
-    {
+    int32_t o = 0;
+    for(int64_t i = 0; i < 100000000000; i++){
         n = 2 + 2;
     }
-    for(int64_t j = 0; j < 50000000000; j++)
-    {
+    for(int64_t j = 0; j < 50000000000; j++){
         m = 2 * 2;
     }
-    for(int64_t k = 0; k < 20000000000; k++)
-    {
+    for(int64_t k = 0; k < 20000000000; k++){
         o = 2 / 2;
     }
     intTime = clock() - intTime;
@@ -36,7 +31,5 @@ clock_t intTime;
     float x = 0.0;
     float y = 0.0;
     float z = 0.0;
-    for(int64_t a = 0; a < 100000000000; a++){
-        x = 2.0 + 2.0;
 
 }
