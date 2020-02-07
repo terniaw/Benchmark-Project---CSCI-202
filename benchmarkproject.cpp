6db39c2e966d24ff5b23dedcf60ca69818c78aac
Ternia Wilson <terniawilson@Ternias-MacBook-Pro.local>
2020-02-06 19:33:35 -0500

    changes, more

--- benchmarkproject.cpp
+++ benchmarkproject.cpp
@@ -14,5 +14,29 @@ clock_t intTime;
     int32_t m = 0;
     int32_t o = 0;         
 
+    for(int64_t i = 0; i < 100000000000; i++)
+    {
+        n = 2 + 2;
+    }
+    for(int64_t j = 0; j < 50000000000; j++)
+    {
+        m = 2 * 2;
+    }
+    for(int64_t k = 0; k < 20000000000; k++)
+    {
+        o = 2 / 2;
+    }
+    intTime = clock() - intTime;
+    double intTime_taken = ((double)intTime)/CLOCKS_PER_SEC;
+    std::cout << intTime_taken << " seconds" << std::endl;
+
+    clock_t floatTime;
+    floatTime = clock();
+    //32-bit float operation benchmark
+    float x = 0.0;
+    float y = 0.0;
+    float z = 0.0;
+    for(int64_t a = 0; a < 100000000000; a++){
+        x = 2.0 + 2.0;
 
 }
